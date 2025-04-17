96cf7dbd4e503c69b58fa4d748f32445efe8f60b
Mlbrands2 <maliwajohn6295@gmail.com>
2025-04-17 18:24:15 +0300

    2

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,18 +1,18 @@
-import { PrismaClient } from "@prisma/client";
-import { seedTaxonomy } from "./taxonomy.seed";
-//import { seedClassifieds } from "./classified.seed";
+// import { PrismaClient } from "@prisma/client";
+// import { seedTaxonomy } from "./taxonomy.seed";
+// //import { seedClassifieds } from "./classified.seed";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-   // await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`
-    //await  seedTaxonomy(prisma);
-    //await seedClassifieds(prisma);
-}
+// async function main() {
+//    // await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`
+//     //await  seedTaxonomy(prisma);
+//     //await seedClassifieds(prisma);
+// }
 
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(() => prisma.$disconnect());
-// This is a seed file for the Prisma Client. It is used to populate the database with initial data.
+// main()
+//   .catch((e) => {
+//     throw e;
+//   })
+//   .finally(() => prisma.$disconnect());
+// // This is a seed file for the Prisma Client. It is used to populate the database with initial data.
